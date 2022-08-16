8957c30047733dd4a0130e4d22b3212d04c79498
csrisravya <92312453+csrisravya@users.noreply.github.com>
2022-08-16 12:24:21 +0530

    Create StressDetection.py

---
+++ StressDetection.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import nltk # nlkt: natural language toolkit is go to APT for Natural Language Processing (NPL) .
+# NPL: concerned with interactions between computers and human(natural) language.How we program computers to understand natural language.
+data = pd.read_csv(r"C:\!1 desktop\akka chelli\akka\reva\Python\StressData2.csv") # 'r' before the path string to address any special characters in the path, such as '\'
+print(data.head(13))  # head():method returns the no of required rows from the dataset.If the no. of rows isn't specified displays first 5 rows 
+print(data.isnull().sum()) # checks for null values
+# Note : it is  necessary to remove all null values to get most efficient results
+
+# Now we clan the dataset by removing stopwords,links,special symbols ,language errors etc
+nltk.download('stopwords') # In natural language processing, useless words (data), are referred to as stop words. 
